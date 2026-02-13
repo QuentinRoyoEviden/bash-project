e46c003e88e1ff8a17e3e908bc39003658608659
Quentin Royo <quentin.royo@eviden.com>
2026-02-13 10:53:40 +0100

    Add correct ref file and new functionalities

--- print_fileref_path.sh
+++ print_fileref_path.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-file_ref="/home/qroyo/bash_project1/ibrefquentin.txt"
+# my switchs will be "iswi1r2s5c0l2" and "iswi1r0s0c0l1"
+# file_ref="/home/qroyo/bash_project1/ibnet.13022026"
+# echo "The way to the reference file is : $file_ref"
 
-echo "The way to the reference file is : $file_ref"
+FILEREF=$1
+SWITCH=$2
+SWITCHTITLE=$(grep $SWITCH $FILEREF | grep "base port")
+NBPORT=$(grep $SWITCH $FILEREF | grep "base port" | awk '{print $2}')
+
+echo "FILEREF is $1 and SWITCH is $2"
+
+PRINT=$(grep -P '(?=.*$SWITCH)(?=.*base port)' -A 12 $FILEREF)
+
+#echo "$SWITCHTITLE"
+echo "$NBPORT"
+echo "$PRINT"
